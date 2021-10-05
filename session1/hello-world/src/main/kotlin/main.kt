e57466e9be8dabab915d1070ef5238c265fb7c60
attilatolnai <attila.tolnai@student.ehb.be>
2021-10-05 11:33:36 +0200

    First commit
    First commit on hello-world

---
+++ session1/hello-world/src/main/kotlin/main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    println("hello world")
+
+    val arrayname = arrayOf("Attila", "Mark", "Jason")
+    for (i in 0..arrayname.size-1)
+    {
+        print("Hello"+arrayname[i])
+    }
+}
