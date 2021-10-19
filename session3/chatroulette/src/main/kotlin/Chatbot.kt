908191ab889efeed63dd055fac76b0655acc0610
attilatolnai <attila.tolnai@student.ehb.be>
2021-10-19 12:50:03 +0200

    Part 3 WIP
    Almost done, but I have an error with the chatbot class

---
+++ session3/chatroulette/src/main/kotlin/Chatbot.kt
@@ -0,0 +1,8 @@
+class Chatbot(): ChatPartner {
+    override fun introduce(){
+        println("Helloooo, my name is Clippy !!")
+    }
+    fun chat(){
+        println("Yay someone wants to talk to me !!")
+    }
+}
--- session3/chatroulette/src/main/kotlin/main.kt
+++ session3/chatroulette/src/main/kotlin/main.kt
@@ -1,7 +1,7 @@
 fun main() {
     println("Session 3 : chatroulette")
 
-    val chatPartners = arrayOf(Teacher("Sam"),Student("Attila"),Comedian ("Mike"))
+    val chatPartners = arrayOf(Teacher("Sam"),Student("Attila"),Comedian ("Mike"),Chatbot())
 
     val chatPartner = chatPartners.random()
     chatPartner.introduce()
