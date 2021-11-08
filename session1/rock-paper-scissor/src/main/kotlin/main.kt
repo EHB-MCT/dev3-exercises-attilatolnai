d866e58311786f33d05a537f501d908ed8d8ed73
attilatolnai <attila.tolnai@student.ehb.be>
2021-11-08 10:41:16 +0100

    Exercise is working
    The user can succesfully play rock-paper-scissors with the computer. Depending on the outcome you will either see a draw, win or lose message

--- session1/rock-paper-scissor/src/main/kotlin/main.kt
+++ session1/rock-paper-scissor/src/main/kotlin/main.kt
@@ -1,12 +1,35 @@
 import kotlin.random.Random
 fun main(){
     println("Let's play rock-paper-scissors !")
-    val option = arrayOf("Rock", "Paper", "Scissor")
+    val option = arrayOf("Rock", "Paper", "Scissors")
     val computerChoice = option.random()
-    println(computerChoice)
-    val playerChoice = selectOption()
+    val playerChoice = getPlayerChoice(option)
+
+    println("The computer chose $computerChoice")
+    println("You chose $playerChoice")
+    determineResult(computerChoice, playerChoice)
 }
 
-fun selectOption(){
+fun getPlayerChoice(option : Array<String>): String?{
+    var playerChoice: String?
+    do {
+        println("Choose: Rock - Paper - Scissors")
+        playerChoice = readLine()
+    } while (!option.contains(playerChoice))
+    return playerChoice
+}
 
+fun determineResult(computerChoice: String, playerChoice: String?){
+    if (computerChoice == playerChoice){
+        println("Draw")
+        return
+    }
+    val winMessage = "You've won"
+    val loseMessage = "You've lost"
+
+    when(computerChoice){
+        "Rock" -> if (playerChoice == "Paper") println(winMessage) else println(loseMessage)
+        "Paper" -> if (playerChoice == "Scissors") println(winMessage) else println(loseMessage)
+        "Scissors" -> if (playerChoice == "Rock") println(winMessage) else println(loseMessage)
+    }
 }
