8b68c1267cd3b7c5a0ebdacb4d7a2230971915d8
attilatolnai <attila.tolnai@student.ehb.be>
2021-11-08 10:58:28 +0100

    Challenge Two done
    The second challenge has been completed. Added a gameOver function to not repeat code

--- session2/adventure-game/src/main/kotlin/main.kt
+++ session2/adventure-game/src/main/kotlin/main.kt
@@ -15,7 +15,38 @@ fun challengeOne(){
     val userAnswer = readLine()
     if (correctAnswer.contains(userAnswer)){
         println("Congratulations, you can proceed to the next question:")
+        challengeTwo()
     }else{
-        println("Unfortunately, failed this challenge you have.")
+        gameOver()
     }
+}
+
+
+fun challengeTwo(){
+    println("Qui-Gon saved Anakin from slavery by playing a game of dice")
+    println("PLay a game of dice and see if you could have saved him?")
+    println("Rules are simple, predict if the dice will be low(1-3) or high(4-6)")
+
+    var userGuess: String
+    do {
+        println("Pick 'low' or 'high'")
+        userGuess = readLine().toString()
+    }while (!setOf<String>("low", "high").contains(userGuess))
+    val diceResult = rollDice(6)
+    println("The dice result was $diceResult")
+    val allowedResults = if (userGuess == "low") setOf<Int>(1,2,3) else setOf(4,5,6)
+        if (allowedResults.contains(diceResult)){
+            println("Looks like you had the force on your side, continue young padawan")
+            //challengeThree()
+    } else{
+        gameOver()
+    }
+}
+
+fun rollDice(sides: Int): Int{
+    return (Math.random() * sides + 1).toInt()
+}
+
+fun gameOver(){
+    println("Unfortunately, failed this challenge you have.")
 }
