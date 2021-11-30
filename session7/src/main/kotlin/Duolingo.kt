c611076480b69b06914a6e46a316444fcb9f846b
attilatolnai <attila.tolnai@student.ehb.be>
2021-11-30 12:08:53 +0100

    1 word works
    getting one word works

--- session7/src/main/kotlin/Duolingo.kt
+++ session7/src/main/kotlin/Duolingo.kt
@@ -4,9 +4,21 @@ class Duolingo() {
         Word("garden", "tuin", "En"),
         Word("dog", "hond", "En"),
         Word("kitchen", "keuken", "En"),
+        Word("car", "wagen", "En"),
     )
 
     fun play(){
-        println(words.random())
+        val currentWords = words.shuffled().take(5)
+        //println(currentWords)
+
+        val selectedWord = currentWords.random()
+        println("Please translate '${selectedWord.original}' to dutch")
+        val userAnswer = readLine()
+
+        if (selectedWord.translated == userAnswer){
+            println("Good job!")
+        }else{
+            println("Try again!")
+        }
     }
 }
